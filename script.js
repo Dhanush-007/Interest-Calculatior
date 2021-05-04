ea91c40b30b9410b6cc05b6af0d2b7355b70ff2b
Dhanush-007 <72206182+Dhanush-007@users.noreply.github.com>
2021-05-04 14:59:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function calculate()
+            {
+                p = document.getElementById("p").value;
+                r = document.getElementById("r").value;
+                n = document.getElementById("n").value;
+                result = document.getElementById("result");
+                
+                result.innerHTML = "The interest is " + (p*n*r/100);
+                
+            }
+          
